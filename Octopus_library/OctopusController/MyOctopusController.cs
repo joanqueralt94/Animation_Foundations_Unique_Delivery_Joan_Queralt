f9eb2b631aa06413bd02815205c0d7844c8184d3
Joan <jqueraltj@hotmail.com>
2020-10-27 15:39:25 +0100

    Initial Commit
    First commit to the repositary

---
+++ Octopus_library/OctopusController/MyOctopusController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace OctopusController
+{
+
+   
+
+    public enum IKMode {GRADIENT, CCD, FABRIK };
+
+
+    public class MyOctopusController 
+    {
+        
+        MyTentacleController[] _tentacles =new  MyTentacleController[4];
+
+        [SerializeField]
+        IKMode _myMode;
+
+        Transform _currentRegion;
+        Transform _target;
+
+        Transform[] _randomTargets;// = new Transform[4];
+
+
+        float _twistMin, _twistMax;
+        float _swingMin, _swingMax;
+
+        #region public methods
+        //DO NOT CHANGE THE PUBLIC METHODS!!
+
+        public float TwistMin { set => _twistMin = value; }
+        public float TwistMax { set => _twistMax = value; }
+        public float SwingMin {  set => _swingMin = value; }
+        public float SwingMax { set => _swingMax = value; }
+        
+
+        public void TestLogging(string objectName)
+        {
+
+           
+            Debug.Log("hello, I am initializing my Octopus Controller in object "+objectName);
+
+            
+        }
+
+
+        //        public void Init(IKMode name, Transform[] tentacleRoots, GameObject[] regions) {
+        public void Init(IKMode name, Transform[] tentacleRoots, Transform[] randomTargets)
+        {
+
+            _myMode = name;
+
+            _tentacles = new MyTentacleController[tentacleRoots.Length];
+
+            // int i = 0;
+            // foreach (Transform t in tentacleRoots)
+            for(int i = 0;  i  < tentacleRoots.Length; i++)
+            {
+
+                _tentacles[i] = new MyTentacleController();
+                _tentacles[i].LoadTentacleJoints(tentacleRoots[i]);
+                i++;
+            }
+
+            _randomTargets = randomTargets;
+            //TODO: use the regions however you need to make sure each tentacle stays in its region
+
+        }
+
+              
+        public void NotifyTarget(Transform target, Transform region)
+        {
+            _currentRegion = region;
+            _target = target;
+
+        }
+
+        public void NotifyShoot() {
+            //TODO. what happens here?
+
+        }
+
+
+        public void UpdateTentacles()
+        {
+            switch (_myMode) {
+                case IKMode.CCD:
+                    //TODO one of the exercises
+                    update_ccd();
+                    break;
+                case IKMode.FABRIK:
+                    update_fabrik();
+                    break;
+                case IKMode.GRADIENT:
+                    update_gradient();
+                    break;
+            }
+
+        }
+
+
+
+
+        #endregion
+
+
+        #region private and internal methods
+        //todo: add here anything that you need
+
+        void update_ccd() {
+
+
+        }
+
+        void update_fabrik()
+        {
+
+
+        }
+
+        void update_gradient()
+        {
+
+
+        }
+
+
+        
+
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ Octopus_library/OctopusController/MyTentacleController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+
+
+namespace OctopusController
+{
+
+    
+    internal class MyTentacleController
+
+    //MAINTAIN THIS CLASS AS INTERNAL
+    {
+
+
+        Transform[] _bones;
+        Transform[] _endEffectorSphere;
+
+
+
+        public Transform[] Bones { get => _bones; }
+
+ 
+
+
+
+
+        //Exercise 1.
+        public Transform[] LoadTentacleJoints(Transform root)
+        {
+            //TODO: add here whatever is needed to find the bones forming the tentacle
+            //you may want to use a list, and then convert it to an array
+            _bones = new Transform[3];
+
+
+
+            //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
+
+
+            return Bones;
+        }
+
+        void MoveRandomly(IKMode mode) {
+
+
+        }
+
+
+
+
+
+
+
+
+}
+}
