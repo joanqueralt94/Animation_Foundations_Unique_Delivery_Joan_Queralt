7bbd4a717010458e870ac9bd97be0debb0a3e311
Joan <jqueraltj@hotmail.com>
2021-01-23 12:14:28 +0100

    Game Shooting  indefenitely implemented with certain force

--- MyUnityProject/Assets/Scripts/Ball.cs
+++ MyUnityProject/Assets/Scripts/Ball.cs
@@ -5,58 +5,99 @@ using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
-    private GameObject _ball;
-    //private float _strenghSliderVelocity;
+
+    [SerializeField]
+    Transform _targetTransform;
+
+    private float _velocity;
     public float _shootStrengh;
     public float _maxStrengh;
-    
-    
+
+    private float _mass = 1f;
+    private bool _isShooting;
 
     public Slider _strenghSlider;
 
-    public Transform _origin;
-    public Transform _target;
+    Vector3 _gravity = new Vector3(0, -4.41f, 0);
 
-    public DrawLine _lineRenderer;
-    
+    private Vector3 _vectorDirection;
+    private Vector3 _initialPosition;
+
+    private Rigidbody _rb;
+
     // Start is called before the first frame update
     void Start()
     {
+        _isShooting = false;
 
-        _ball = GameObject.Find("Ball");
-        //_strenghSliderVelocity = 1f;
         _shootStrengh = 0f;
         _maxStrengh = 100f;
-        
+            
+        _rb = GetComponent<Rigidbody>();
+        _initialPosition = transform.position;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            forceIncrement();
-            //Debug.Log("Spacebar being pressed and force value is: " + _shootStrengh);
+        forceIncrement();
 
-            if (_shootStrengh >= _maxStrengh)
-            {
-                _shootStrengh = 100f;
-            }
+        _strenghSlider.value = _shootStrengh;
+        if((transform.position - (transform.position + _vectorDirection * _velocity * Time.deltaTime)).z > (transform.position - _targetTransform.position).z)
+        {
+            _isShooting = false;
+            transform.position = _initialPosition;
+        }
+        
+        if (_isShooting)
+        {
+            _rb.isKinematic = true;
+            _vectorDirection = (_targetTransform.position - transform.position).normalized;
+            _velocity = _shootStrengh / _mass;
+            transform.position += (_vectorDirection * _velocity) * Time.deltaTime + _gravity * Time.deltaTime;
             
         }
         else
         {
-            _shootStrengh = 0f;   
+            _rb.isKinematic = false;
         }
-
-        _strenghSlider.value = _shootStrengh;
-
-        //_ball.transform.Translate(_lineRenderer.transform.forward * _shootStrengh);
-
     }
+
 
     public void forceIncrement()
     {
-        _shootStrengh++;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _shootStrengh++;
+            if (_shootStrengh >= _maxStrengh)
+            {
+                _shootStrengh = 100f;
+            }
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+            _isShooting = true;
+            StartCoroutine(ReturnGame());
+            
+
+        }
+
+    }
+
+    private IEnumerator ReturnGame()
+    {
+        yield return new WaitForSeconds(1f);
+        _isShooting = false;
+        _shootStrengh = 0f;
+        transform.position = _initialPosition;
+
     }
 }
+
+   
+
